810e8a708ac248fd99f8ceb58c9929b752507c35
24267js-a11y <24267js@hvhs.school.nz>
2026-04-23 03:00:03 +0000

    Apr 23, 2026, 3:00 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,19 @@
-/* Paste your code from task 1 here */
+/****************************
+Name of task: Introduction
+****************************/
+console.log("Running t01_introduction.js");
+console.log("utftrfy")
+
+//Variables
+
+
+/****************************
+  Main code
+****************************/
+
+
+
+
+/*************************** 
+ Functions
+***************************//* Paste your code from task 1 here */
