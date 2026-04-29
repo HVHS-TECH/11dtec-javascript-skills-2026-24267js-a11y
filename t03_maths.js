7d7f95e49255a438a666186a1c7492daa4b0bef2
24267js-a11y <24267js@hvhs.school.nz>
2026-04-29 01:13:38 +0000

    Apr 29, 2026, 1:13 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,23 @@
 /* Paste your code from task 2 here */
+/****************************
+Name of task: Variables
+****************************/
+console.log("Running t03_maths.js");
+
+//Variables
+var name = "James";
+var age = 26;
+
+var year = 2026;
+let money =1000;
+
+/****************************
+  Main code
+****************************/
+console.log("Hi " + name + ", as of " + year + " you are " + age + " years old and you have $" + money + " dollars.");
+
+
+
+/*************************** 
+ Functions
+***************************/ 
