e2d872ef68c5d44f8c4f9520c03898b9b868f54f
24267js-a11y <24267js@hvhs.school.nz>
2026-05-03 21:20:43 +0000

    May 3, 2026, 9:20 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,6 +1,6 @@
 /* Paste your code from task 3, Maths here */
 /****************************
-Name of task: Maths
+Name of task: Javascript and HTML
 ****************************/
 
 
@@ -18,7 +18,7 @@ let equation = money / 2 + 3;
 /****************************
   Main code
 ****************************/
-console.log("Running t03_maths.js");
+console.log("Running t05_JavaScript_and_HTML.js");
 
 console.log("Hi " +username+ ", as of " + year + " you are " + age + " years old. You were born in " + dateofbirth + ". In 10 years you will be " + ageAdd + " and you have $" + money + " dollars. You spend half of your money, you now have $" + total + " dollars. Then you get $3, now you have $" + equation + " dollars." );
 
