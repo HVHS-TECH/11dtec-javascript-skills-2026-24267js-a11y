f87d78e90e0f30f1d51bb838ef3cd7c865f05496
24267js-a11y <24267js@hvhs.school.nz>
2026-05-04 22:33:01 +0000

    May 4, 2026, 10:33 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -17,7 +17,7 @@ let equation = money / 2 + 3;
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
-OUTPUT.innerHTML = "<h1>HHELLO PERSONNNNNN.</h1>";
+OUTPUT.innerHTML = "<h1>GIVE ME CHICKENNNNNNNNNNNNNNNN.</h1>";
 
 /****************************
   Main code
