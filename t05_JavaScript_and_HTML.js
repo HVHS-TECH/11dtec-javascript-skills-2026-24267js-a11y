a1661d246d481ebb6e4db6c98d1576ef77c5cc63
24267js-a11y <24267js@hvhs.school.nz>
2026-05-04 22:25:00 +0000

    May 4, 2026, 10:25 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -15,6 +15,10 @@ let money =1000;
 let total = money / 2;
 let equation = money / 2 + 3;
 
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+OUTPUT.innerHTML = "<p>Hello World!.</p>";
+
 /****************************
   Main code
 ****************************/
@@ -22,11 +26,6 @@ console.log("Running t05_JavaScript_and_HTML.js");
 
 console.log("Hi " +username+ ", as of " + year + " you are " + age + " years old. You were born in " + dateofbirth + ". In 10 years you will be " + ageAdd + " and you have $" + money + " dollars. You spend half of your money, you now have $" + total + " dollars. Then you get $3, now you have $" + equation + " dollars." );
 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "<h2>hello world</h2>";
-OUTPUT.innerHTML = "<p>Hello " + username + "</p>";
-OUTPUT.innerHTML = "<p>Second Paragraph Line.</p>";
-OUTPUT.innerHTML = "<p>Hello World.</p>";
 
 
 
