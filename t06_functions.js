ab0b9242adc4bf0e731295de808638e24171c5e6
24267js-a11y <24267js@hvhs.school.nz>
2026-05-05 23:21:58 +0000

    May 5, 2026, 11:21 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,39 @@
 /* Paste your code from task 5 here */
+/****************************
+Name of task: Javascript and HTML
+****************************/
+
+
+//Variables
+var username = "James";
+var age = 26;
+
+var year = 2026;
+var dateofbirth = 2000;
+let ageAdd = age + 10;
+let money =1000;
+let total = money / 2;
+let equation = money / 2 + 3;
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+OUTPUT.innerHTML = "<h1>ADDED BY JAVASCRIPT.</h1>";
+OUTPUT.innerHTML += "<p>" + "Hello " + username + ", welcome!</p>";
+OUTPUT.innerHTML += "<p>" + "Since you are so rich...." + "</p>";
+OUTPUT.innerHTML += "<p>" + "CAN YOU BUY FRIED CHICKENNNNNNNNNNNNN?!" + "</p>";
+OUTPUT.innerHTML += "<p>" + "Hi " + username + ", as of " + year + " you are " + age + " years old. You were born in " + dateofbirth + ". In 10 years you will be " + ageAdd + " and you have $" + money + " dollars. You spent half of your money, you now have $" + total + " dollars. Then you get $3, now you have $" + equation + " dollars." + "</p>";
+
+
+/****************************
+  Main code
+****************************/
+console.log("Running t05_JavaScript_and_HTML.js");
+
+console.log("Hi " +username+ ", as of " + year + " you are " + age + " years old. You were born in " + dateofbirth + ". In 10 years you will be " + ageAdd + " and you have $" + money + " dollars. You spend half of your money, you now have $" + total + " dollars. Then you get $3, now you have $" + equation + " dollars." );
+
+
+
+
+/*************************** 
+ Functions
+***************************/ 
