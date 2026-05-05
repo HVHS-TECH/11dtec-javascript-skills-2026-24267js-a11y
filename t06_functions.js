6a5ba5e21ea1781a3ad861d2c1c79ec827f42221
24267js-a11y <24267js@hvhs.school.nz>
2026-05-05 23:38:53 +0000

    May 5, 2026, 11:38 PM

--- t06_functions.js
+++ t06_functions.js
@@ -31,11 +31,12 @@ console.log("Running t06_functions.js");
 
 console.log("Hi " +username+ ", as of " + year + " you are " + age + " years old. You were born in " + dateofbirth + ". In 10 years you will be " + ageAdd + " and you have $" + money + " dollars. You spend half of your money, you now have $" + total + " dollars. Then you get $3, now you have $" + equation + " dollars." );
 
-
+writeline("Welcome to the shop");
 
 
 /*************************** 
  Functions
 ***************************/ 
 function writeline() {
-  OUTPUT.innerHTML +=
+  OUTPUT.innerHTML +="<p>Less Code Matters</p>";
+}
