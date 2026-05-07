dfc6b21d35097e2364e6b75840e6570efbfcbd40
24267js-a11y <24267js@hvhs.school.nz>
2026-05-07 03:12:55 +0000

    May 7, 2026, 3:12 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -36,8 +36,9 @@ console.log("Running t07_functions_parameters.js");
 console.log("Hi " +username+ ", as of " + year + " you are " + age + " years old. You were born in " + dateofbirth + ". In 10 years you will be " + ageAdd + " and you have $" + money + " dollars. You spend half of your money, you now have $" + total + " dollars. Then you get $3, now you have $" + equation + " dollars." );
 
 writeline("Welcome");
-writeline("Chocolate Bar: $4");
 
+displayProduct("Chocolate Bar: $4");
+ 
 
 /*************************** 
  Functions
@@ -48,6 +49,5 @@ function writeline() {
 
 function displayProduct(_product,_price) {
     OUTPUT.innerHTML += "<p>Chocolate Bar: " +_chocolatebar+ " Price:$4 </p>";
-
 }
 
