d1832e5c9a75b8553310f63a363d11ac3196eeec
24267js-a11y <24267js@hvhs.school.nz>
2026-05-07 02:31:48 +0000

    May 7, 2026, 2:31 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,6 +1,6 @@
 /* Paste your code from task 6 here */
 /****************************
-Name of task: Functions
+Name of task: Functions_parameters
 ****************************/
 
 
@@ -29,7 +29,7 @@ OUTPUT.innerHTML += "<p>" + "Hi " + username + ", as of " + year + " you are " +
 /****************************
   Main code
 ****************************/
-console.log("Running t06_functions.js");
+console.log("Running t07_functions_parameters.js");
 
 console.log("Hi " +username+ ", as of " + year + " you are " + age + " years old. You were born in " + dateofbirth + ". In 10 years you will be " + ageAdd + " and you have $" + money + " dollars. You spend half of your money, you now have $" + total + " dollars. Then you get $3, now you have $" + equation + " dollars." );
 
