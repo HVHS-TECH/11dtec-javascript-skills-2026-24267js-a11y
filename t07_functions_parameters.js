0cd2412edfb869b7546f2ff1154a63eff266007e
24267js-a11y <24267js@hvhs.school.nz>
2026-05-07 02:49:05 +0000

    May 7, 2026, 2:49 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -45,7 +45,7 @@ function writeline() {
   OUTPUT.innerHTML +="<p>Welcome to the shop</p>";
 }
 
-function displayProduct(chocolatebar) {
-    OUTPUT.innerHTML += "<p>We have " + chocolatebar + " in stock!<p/>";
+function displayProduct(_chocolatebar) {
+    
 }
 
