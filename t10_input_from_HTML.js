ef579f74b2bdf971657bbf753ce109df2142c55c
24267js-a11y <24267js@hvhs.school.nz>
2026-05-14 02:10:17 +0000

    May 14, 2026, 2:10 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -59,7 +59,7 @@ OUTPUT.innerHTML += "Hi Angel"
 }
 
 function getFormInput(){
-let user = NAME_FIELD.value
+let username = NAME_FIELD.value;
 OUTPUT.innerHTML = "<p>Your name is"+ userName +"</p>";
 }
 
