3f7cfb0bf110904c1d3a84d0a05a9b21cf4c2e2c
24267js-a11y <24267js@hvhs.school.nz>
2026-05-31 01:01:24 +0000

    May 31, 2026, 1:01 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -74,9 +74,9 @@ OUTPUT.innerHTML = "<p>You have" + money + "pocket money.<p>";
 }
 
 
-if (_product > MONEY_FIELD) {
+if (displayProduct > money) {
   OUTPUT.innerHTML += "<p><br><br>Sorry, you don't have enough money to buy this product.<br><br><br></p>";
 }
-else if (_product == MONEY_FIELD) {
+else if (displayProduct == money) {
   OUTPUT.innerHTML += "<p><br><br>You have just enough money to buy this product but you will have no money left.<br><br><br></p>";
 }
