d7e893003cb714fb72c55707e3fef951a546f8d6
24267js-a11y <24267js@hvhs.school.nz>
2026-06-01 05:57:06 +0000

    dcsdc

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -43,7 +43,7 @@ displayProduct("Chocolate Bar: ", 4);
 displayProduct("Chips: ", 3);
 displayProduct("Drink: ", 2.50);
 
-getFormInput(Userage);
+getFormInput(age);
 getFormInput(money);
 /*************************** 
  Functions
@@ -64,8 +64,8 @@ OUTPUT.innerHTML += "Hi Angel"
 function getFormInput(){
 const AGE_FIELD = document.getElementById("ageField");
 console.log(AGE_FIELD)
-let Userage = AGE_FIELD.value; 
-OUTPUT.innerHTML = "<p>Your age is" + Userage + "years old.<p>";
+let age = AGE_FIELD.value; 
+OUTPUT.innerHTML = "<p>Your age is" + age + "years old.<p>";
 }
 
 function getFormInput(){
