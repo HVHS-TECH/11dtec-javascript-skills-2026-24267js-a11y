28d96ae2178907c7459c7b78fc601f1fdfe4c7bc
24267js-a11y <24267js@hvhs.school.nz>
2026-06-24 19:41:16 +0000

    Jun 24, 2026, 7:41 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,10 @@
 /* Paste your code from the last task */
+console.log("Running t15_for_loop.js");
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+function getBottleInput() {
+    let bottleamount = number(BOTTLE_FIELD.value).tofixed(0);
+    for(let i = 1; i <= bottleamount; i++) {
+        
+}
