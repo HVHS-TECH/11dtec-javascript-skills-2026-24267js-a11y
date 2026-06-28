f7402e99e30235a59cd11461e00b3e0b616a13c1
24267js-a11y <24267js@hvhs.school.nz>
2026-06-28 06:05:15 +0000

    edef

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -12,5 +12,10 @@ console.log("Loop finished");
 
 console.log("100 bottles of milk :");
 for (let bottle = 100; bottle > 90; bottle--) {
-    console.log(bottle + " bottles of milk on the wall");
+    console.log(bottle + " bottles of milk on the wall,");
+    console.log(bottle + " bottles of milk,");
+    console.log("if one of those bottles should happen to fall...");
+
 }
+
+console.log("No more bottles of milk on the wall, no more bottles of milk.");
